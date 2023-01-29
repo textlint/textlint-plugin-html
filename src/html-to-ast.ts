106d50d09fb77dc75122e8d8a6d8816481e35a2d
azu <azuciao@gmail.com>
2023-01-29 19:59:31 +0900

    fix: do not use Html

--- src/html-to-ast.ts
+++ src/html-to-ast.ts
@@ -5,7 +5,7 @@ import rehypeParse from 'rehype-parse'
 import traverse, { TraverseContext } from "traverse";
 import { StructuredSource } from "structured-source";
 import { nodeTypes, tagNameToType } from "./mapping.js";
-import { Element } from "hast";
+import { Element, RootContent } from "hast";
 
 /**
  * Remove undocumented properties on TxtNode from node
@@ -22,30 +22,34 @@ import { Element } from "hast";
 //     });
 // }
 
-function mapNodeType(node: Element, parent: TraverseContext | undefined) {
+function mapNodeType(node: RootContent, parent: TraverseContext | undefined) {
     if (parent) {
         const parentNode = parent.parent?.node;
         if (parentNode?.tagName === "script" || parentNode?.tagName === "style") {
             return "CodeBlock";
         }
     }
-    if (node.tagName && node.type === "element") {
+    if (node.type === "element") {
         // @ts-expect-error: tagName is string
         const mappedType = tagNameToType[node.tagName];
         if (mappedType) {
             // p => Paragraph...
             return mappedType;
         } else {
-            // other element is "Html"
-            return "Html";
+            // other case, return original tagName
+            return node.tagName;
         }
+    } else if (node.type === "doctype") {
+        return "doctype";
     } else if (node.type in nodeTypes) {
         // mappable node type
         // text => Str
         return nodeTypes[node.type];
     }
-    // It is not defined in textlint's AST
-    return "Html";
+    // The other node is original node.type because it is not defined in textlint's AST
+    // Almost rule should not handle this node.
+    // https://github.com/textlint/textlint-plugin-html/issues/19
+    return node.type;
 }
 
 export type ParseOptions = {
@@ -81,20 +85,11 @@ export function parse(html: string, options?: ParseOptions) {
             }
             // avoid conflict <input type="text" />
             // AST node has type and position
-            if (node.type && node.position) {
+            if (node.type) {
                 // case: element => Html or ...
                 node.type = mapNodeType(node, this.parent);
-            } else if (node.type === "root") {
-                // FIXME: workaround, should fix hast
-                node.type = nodeTypes[node.type as "root"];
-                const position = src.rangeToLocation([0, html.length]);
-                // reverse adjust
-                node.position = {
-                    start: { line: position.start.line, column: position.start.column + 1 },
-                    end: { line: position.end.line, column: position.end.column + 1 }
-                };
             } else {
-                node.type = "Html" as const;
+                // node.type = "Html" as const;
             }
             // map `range`, `loc` and `raw` to node
             if (typeof node.position === "object") {
--- src/mapping.ts
+++ src/mapping.ts
@@ -18,25 +18,6 @@ export const tagNameToType = {
 
 export const nodeTypes = {
     "root": "Document",
-    "paragraph": "Paragraph",
-    "blockquote": "BlockQuote",
-    "listItem": "ListItem",
-    "list": "List",
-    "Bullet": "Bullet", // no need?
-    "heading": "Header",
-    "code": "CodeBlock",
     "comment": "Comment",
-    "HtmlBlock": "Html",
-    "ReferenceDef": "ReferenceDef",
-    "horizontalRule": "HorizontalRule",
-    // inline block
     'text': 'Str',
-    'break': 'Break',
-    'emphasis': 'Emphasis',
-    'strong': 'Strong',
-    'html': 'Html',
-    'link': 'Link',
-    'image': 'Image',
-    'inlineCode': 'Code',
-    'yaml': 'Yaml'
 } as const;
